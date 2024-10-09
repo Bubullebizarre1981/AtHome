8579914bccbc43f84a56a47cf7d46e718fa8fbb7
Bubullebizarre1981 <loic.jochmans@gmail.com>
2024-10-09 10:51:51 +0200

    Initial Commit

---
+++ AtHomeAppTests/AtHomeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AtHomeAppTests.swift
+//  AtHomeAppTests
+//
+//  Created by Apprenant 171 on 09/10/2024.
+//
+
+import Testing
+@testable import AtHomeApp
+
+struct AtHomeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
