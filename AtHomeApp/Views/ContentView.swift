c9592410086719304bcd922b69209daa25367561
Pierre C <cilluffo.jeuxvideo@outlook.fr>
2024-10-21 09:10:53 +0200

    Correction Default Project

---
+++ AtHomeApp/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  AtHomeApp
+//
+//  Created by Apprenant 171 on 09/10/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World !")
+    }
+}
+
+#Preview {
+    ContentView()
+}
