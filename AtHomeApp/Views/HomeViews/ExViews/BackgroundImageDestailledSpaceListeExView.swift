d092d40229e35705447f568982f95d643e7c3b7f
Gregor <gregorleguellec@hotmail.fr>
2024-10-23 16:19:54 +0200

    Création des structures BackgroundImageDetailledSpace et ButtonDetailledSpaceListeExView pour moduler et simpliier le code dans DetailledSpaceListeView

---
+++ AtHomeApp/Views/HomeViews/ExViews/BackgroundImageDestailledSpaceListeExView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundImageDestailledSpaceListeExView.swift
+//  AtHomeApp
+//
+//  Created by Apprenant 176 on 23/10/2024.
+//
+
+import SwiftUI
+
+struct BackgroundImageDestailledSpaceListeExView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundImageDestailledSpaceListeExView()
+}
---
+++ AtHomeApp/Views/HomeViews/ExViews/ButtonDetailledSpaceListeExView.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonDetailledSpaceListeExView.swift
+//  AtHomeApp
+//
+//  Created by Apprenant 176 on 23/10/2024.
+//
+
+import Foundation
